22dbcce33080561de62ebcf3817baeea9ee1d028
2530323-spec <2530323@upv.edu.mx>
2025-10-29 10:31:37 -0600

    feat:understanding strings

--- src/understanding_strings.py
+++ src/understanding_strings.py
@@ -14,3 +14,36 @@ Ejemplos:
 "El lenguaje ´Python´ lleva el nombre por Monty Python, NO por la serpiente"
 
 """
+name= "clase de programacion"
+print (name)
+
+#title
+print(name.title())
+
+print (name)
+
+
+"""
+Un metodo es una accion que python puede realizar en un
+fragmento de datos o sobre una variable.
+
+    El punto . despues de una variable
+    seguido del metodo title() dice que se tiene que ejecutar el metodo title de la variable name.
+
+    Todos los metodos van seguidos de parentesis
+    porque en ocasiones necesitan informacion adicional
+    para funcionar, la cual iria dentro de los parentesis.
+
+    En esta ocasion, el metodo .title() no requiere informacion adicional para funcionar
+"""
+
+print("metodo upper:",name.upper())
+print ("Metodo lower:", name.lower())
+
+#Concatenion de Strings
+
+first_name= "santiago"
+last_name="chavez"
+full_name = first_name +" "+ last_name
+print(full_name)
+print(full_name.title())
