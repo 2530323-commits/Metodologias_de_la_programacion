a75b2f15ba0cdad1153a5dbe09b3b2ddad4e2323
2530323-spec <2530323@upv.edu.mx>
2025-11-03 10:41:20 -0600

    feat:understanding strings

---
+++ src/understanding_number.py
@@ -0,0 +1,48 @@
+#Numbers
+
+"""
+    Enteros
+
+        Los podemos sumar (+), restar (-), multiplicar (*)
+        y dividir (/)
+
+"""
+number_1 = 35
+number_2 = 15
+suma = number_1 + number_2
+resta = number_1 - number_2
+multiplicacion = number_1 * number_2
+division = number_1 / number_2
+potencia=number_1**2
+modulo= number_1%number_2
+print ("suma: ",suma,  type(suma))
+print ("resta: ",resta,  type (resta))
+print ("multiplicacion: ",multiplicacion, type (multiplicacion))
+print ("division: ", division, type (division))
+print ("potencia: ",potencia, type (potencia))
+print ("modulo: ",modulo, type (modulo))
+
+"""
+ Floats
+    Pyhon llama Floats a cualquier numero con un punto decimal.
+
+    Los podemos sumar (+), restar (-), multiplicar (*)
+        y dividir (/)
+"""
+print(0.1+0.1)
+print(0.02-0.2)
+print (0.1*2)
+print(2*0.2)
+
+#Imprimir la edad de alguien
+age = 33
+message = "charly tiene " + str(age) + " años."
+print(message)
+
+
+"""
+TypeError: Esto significa que Python 
+no puede reconocer el tipo de informacion 
+que esta utilizando.
+
+"""
