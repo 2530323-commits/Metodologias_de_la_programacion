1ea287b3a21657faceb54ac5e9fb7f599f92fda9
2530323-spec <2530323@upv.edu.mx>
2025-11-28 07:53:47 -0600

    feat:hoy aprendi a usar functions

--- src/understanding_functions.py
+++ src/understanding_functions.py
@@ -23,3 +23,26 @@ def greetting_christopher():
      print("Hello Christopher")
     
 greetting_christopher()
+
+#Ejemplo de una funcion que genere el nombre completo 
+# de una persona y que lo regrese 
+
+def create_full_name(first_name, last_name, middle_name=""):
+    full_name = f"{first_name.strip()}{middle_name.strip()} {last_name.strip()}".title()
+    return full_name
+first_name = input("dame tu primer nombre: ")
+middle_name = input("dame tu segundo nombre: (Si no tiene segundo ombre, da enter) ")
+last_name = input ("dame tu apellido: ")
+
+generated_fullname = create_full_name(
+    first_name.strip().lower(),
+    last_name.strip().lower())
+print(generated_fullname)
+
+
+generated_fullname_2(
+    middle_name = user_middle_name,
+    first_name = user_first_name,
+    last_name = user_last_name
+)
+print(generated_fullname_2)    
